b26ad7082c08125b8efc243788a12029a8c163bb
Michael <michaellee@richitech.com.tw>
2022-12-31 19:06:47 +0800

    優化抽籤系統

--- LineBot/Domain/TextEvent/Drawstraws/DrawStraws.cs
+++ LineBot/Domain/TextEvent/Drawstraws/DrawStraws.cs
@@ -17,8 +17,7 @@ namespace LineBot.Domain.TextEvent
         {
             if (!Lots.Any())
             {
-                Lots = LotTypes.SelectMany(data => Enumerable.Repeat((data.type, data.describe), data.count))
-                               .ToList();
+                RandomLots();
             }
             Random rand = new Random(Guid.NewGuid().GetHashCode());
             (string type, string describe) lot = Lots[rand.Next(0, Lots.Count)];
@@ -26,16 +25,35 @@ namespace LineBot.Domain.TextEvent
             ReplyText(new List<string> { lot.type, lot.describe });
         }
 
-        public List<(int count, string type, string describe)> LotTypes = new List<(int, string, string)>
+        private void RandomLots()
         {
-            (3, "大吉","萬人之上，老鼠之下。"),
-            (10,"上上","當我眼睛閉上時，我就會什麼都看不到。"),
-            (13,"上吉","在美國就連小學生都會說流利的美語，而你什麼都不會。"),
-            (1, "上平","當你正在努力工作的時候，地球另一半邊的人正在努力睡覺。"),
-            (9 ,"中吉","如果你沒有努力過，就會發現你真的什麼都做不到。"),
-            (36,"中平","早安，路人甲。"),
-            (1 ,"中中","對你而言，我就是過客，而你什麼都不是。"),
-            (27,"下下","連一隻草履蟲都不如。"),
-        };
+            Lots = LotTypes().SelectMany(data => Enumerable.Repeat((data.type, data.describe), data.count))
+                                                           .ToList();
+
+            Random rand = new Random(Guid.NewGuid().GetHashCode());
+
+            for (int i = 0; i < Lots.Count; i++)
+            {
+                int tempPosition = rand.Next(0, Lots.Count);
+                var temp = Lots[i];
+                Lots[i] = Lots[tempPosition];
+                Lots[tempPosition] = temp;
+            }
+        }
+
+        public List<(int count, string type, string describe)> LotTypes()
+        {
+            return new List<(int count, string type, string describe)>
+                    {
+                        (3, "大吉","萬人之上，老鼠之下。"),
+                        (10,"上上","當我眼睛閉上時，我就會什麼都看不到。"),
+                        (13,"上吉","在美國就連小學生都會說流利的美語，而你什麼都不會。"),
+                        (1, "上平","當你正在努力工作的時候，地球另一半邊的人正在努力睡覺。"),
+                        (9 ,"中吉","如果你沒有努力過，就會發現你真的什麼都做不到。"),
+                        (36,"中平","早安，路人甲。"),
+                        (1 ,"中中","對你而言，我就是過客，而你什麼都不是。"),
+                        (27,"下下","連一隻草履蟲都不如。")
+                    };
+        }
     }
 }
