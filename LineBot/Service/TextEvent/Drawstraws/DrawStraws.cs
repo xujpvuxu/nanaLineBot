8faa500c356042604177220c128f96c3f82d35a7
Michael <michaellee@richitech.com.tw>
2023-01-01 08:13:37 +0800

    抽籤改從負能量網站抓取並洗亂放入籤中

--- LineBot/Service/TextEvent/Drawstraws/DrawStraws.cs
+++ LineBot/Service/TextEvent/Drawstraws/DrawStraws.cs
@@ -1,5 +1,6 @@
 ﻿using LineBot.DTO.Webhook;
 using LineBot.Interfaces;
+using System.Text.RegularExpressions;
 
 namespace LineBot.Domain.TextEvent
 {
@@ -11,7 +12,8 @@ namespace LineBot.Domain.TextEvent
 
         public string Pattern { get; set; } = "^抽籤$";
 
-        public static List<(string type, string describe)> Lots = new List<(string, string)>();
+        private string NegitiveUri = @"https://arielhsu.tw/negative-energy-sentence/";
+        private static List<(string type, string describe)> Lots = new List<(string, string)>();
 
         public void Result()
         {
@@ -27,33 +29,85 @@ namespace LineBot.Domain.TextEvent
 
         private void RandomLots()
         {
-            Lots = LotTypes().SelectMany(data => Enumerable.Repeat((data.type, data.describe), data.count))
-                                                           .ToList();
+            List<string> negitive = GetNegativeEnergy();
+            ForRandom(negitive);
 
+            Lots = LotTypes().SelectMany(data => Enumerable.Repeat(data.type, data.count))
+                             .Zip(negitive, (x, y) => (x, y))
+                             .ToList();
+            ForRandom(Lots);
+        }
+
+        /// <summary>
+        /// 洗亂
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        private void ForRandom<T>(List<T> source)
+        {
             Random rand = new Random(Guid.NewGuid().GetHashCode());
 
-            for (int i = 0; i < Lots.Count; i++)
+            for (int i = 0; i < source.Count; i++)
             {
-                int tempPosition = rand.Next(0, Lots.Count);
-                var temp = Lots[i];
-                Lots[i] = Lots[tempPosition];
-                Lots[tempPosition] = temp;
+                int tempPosition = rand.Next(0, source.Count);
+
+                var temp = source[i];
+                source[i] = source[tempPosition];
+                source[tempPosition] = temp;
             }
         }
 
-        public List<(int count, string type, string describe)> LotTypes()
+        /// <summary>
+        /// 抽籤個數
+        /// </summary>
+        /// <returns></returns>
+        private List<(int count, string type)> LotTypes()
         {
-            return new List<(int count, string type, string describe)>
+            return new List<(int count, string type)>
                     {
-                        (3, "大吉","萬人之上，老鼠之下。"),
-                        (10,"上上","當我眼睛閉上時，我就會什麼都看不到。"),
-                        (13,"上吉","在美國就連小學生都會說流利的美語，而你什麼都不會。"),
-                        (1, "上平","當你正在努力工作的時候，地球另一半邊的人正在努力睡覺。"),
-                        (9 ,"中吉","如果你沒有努力過，就會發現你真的什麼都做不到。"),
-                        (36,"中平","早安，路人甲。"),
-                        (1 ,"中中","對你而言，我就是過客，而你什麼都不是。"),
-                        (27,"下下","連一隻草履蟲都不如。")
+                        (3, "大吉"),
+                        (10,"上上"),
+                        (13,"上吉"),
+                        (1, "上平"),
+                        (9 ,"中吉"),
+                        (36,"中平"),
+                        (1 ,"中中"),
+                        (27,"下下"),
                     };
+        }
+
+        /// <summary>
+        /// 網站中抓取負能量
+        /// </summary>
+        /// <returns></returns>
+        private List<string> GetNegativeEnergy()
+        {
+            List<string> negatives = new List<string>();
+
+            HttpClient client = new HttpClient();
+            string content = client.GetStringAsync(NegitiveUri).Result;
+            string pattern = $@"font-size: 20px; color: #000000;"">(?<negtive>.*?)<";
+            var ete = Regex.Matches(content, pattern);
+            List<string> t = Regex.Matches(content, pattern)
+                                  .OfType<Match>()
+                                  .Select(data => Regex.Replace(data.Groups["negtive"].Value, @"[\d]{1,2}、", string.Empty))
+                                  .Where(data => !string.IsNullOrWhiteSpace(data)).ToList();
+            negatives = t.Select(
+                                        (data, i) =>
+                                        {
+                                            string result = data;
+                                            if (i == 88)
+                                            {
+                                                //特別處理
+                                                result = $"{t[88]}{t[89]}";
+                                            }
+                                            return result;
+                                        })
+                                  .Where((data, i) => i != 89)
+                                  .Take(98).ToList();
+            negatives.Add("努力不保證成功，偷懶保證輕鬆！");
+            negatives.Add("世上無難事，只要肯放棄");
+            return negatives;
         }
     }
 }
