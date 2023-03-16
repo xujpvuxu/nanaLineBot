bc22e42d037532b5dbd6df718b0bd249e73dd92f
Michael <michaellee@richitech.com.tw>
2023-03-16 18:37:37 +0800

    Add FishPictures

--- LineBot/DAO/SevenPicturesDAO.cs
+++ LineBot/DAO/SevenPicturesDAO.cs
@@ -4,5 +4,6 @@
     {
         public static List<string> NanaPicture { get; set; } = new List<string>();
         public static List<string> SnowPicture { get; set; } = new List<string>();
+        public static List<string> FishPicture { get; set; } = new List<string>();
     }
 }
---
+++ LineBot/Service/MessageEventSpace/Pictures/FishPicture.cs
@@ -0,0 +1,27 @@
+﻿using LineBot.DAO;
+using LineBot.Domain.MessageEventSpace.Base;
+using LineBot.DTO.Messages;
+using LineBot.DTO.Messages.Request;
+using LineBot.DTO.Webhook;
+using LineBot.Interfaces;
+using System.Text.RegularExpressions;
+
+namespace LineBot.Domain.MessageEventSpace
+{
+    public class FishPicture : BasePicture, IMessageEventSpace
+    {
+        public string Pattern { get; set; } = "^鮭魚$";
+
+        public FishPicture(WebhookEventDto eventObject) : base(eventObject)
+        {
+            SheetID = "1U7PDQJDnJTyX6Y-kbfEqI5IaT2a0sM3qYrXi96iAGaA";
+            SheetWorkName = "Fish";
+        }
+
+        public void Result()
+        {
+            (bool isPicture, string replyMessge) = IsGetPicture(SevenPicturesDAO.FishPicture, SheetID, SheetWorkName);
+            SendPicture(isPicture, replyMessge, SevenPicturesDAO.FishPicture);
+        }
+    }
+}
