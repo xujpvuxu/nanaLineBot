a88b8242f96de46d4b2595f4612f64872a5e3d01
Michael <Michael>
2024-12-30 23:25:31 +0800

    新增 EatHelper 類別

---
+++ LineBot/Service/MessageEventSpace/Nutrition/EatHelper.cs
@@ -0,0 +1,21 @@
+﻿using LineBot.Domain.MessageEventSpace.Base;
+using LineBot.DTO.Webhook;
+using LineBot.Interfaces;
+using System.Text.RegularExpressions;
+
+namespace LineBot.Domain.MessageEventSpace
+{
+    public class EatHelper : BasePicture, IMessageEventSpace
+    {
+        public EatHelper(WebhookEventDto eventObject) : base(eventObject)
+        {
+        }
+
+        public string Pattern { get; set; } = $@"zzHelper";
+
+        public void Result()
+        {
+            ReplyText($"zz 份數 熱量(Kcl) 蛋白質(g) 碳水化合物(g)");
+        }
+    }
+}
